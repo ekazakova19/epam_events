811c010ea396902e6aa9b80346ffe5447c18bfba
ekazakova19 <49072165+ekazakova19@users.noreply.github.com>
2020-05-03 11:24:19 +0400

    Move all to test folder

--- src/test/java/testSteps/EventsPageSteps.java
+++ src/test/java/testSteps/EventsPageSteps.java
@@ -1,8 +1,8 @@
 package testSteps;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.EventsPage;
